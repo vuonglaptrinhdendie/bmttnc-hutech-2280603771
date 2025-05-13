26f605f70ec80a376eff0f96747cdffc18e6b222
Vuonglaptrinhdendie <nguuyenthanhvuong1303@gmail.com>
2025-05-13 11:37:43 +0700

    fix bug at line 5

--- lap_01/ex02/ex02_10.py
+++ lap_01/ex02/ex02_10.py
@@ -2,5 +2,5 @@ def dao_nguoc_chuoi(chuoi):
     return chuoi[::-1]
 
 # Sử dụng hàm và in kết quả
-input_string = input("Mời nhập chuỗi cần đảo ngược: ")
+input_string = input("Mời: ")
 print("Chuỗi đảo ngược là:", dao_nguoc_chuoi(input_string))
